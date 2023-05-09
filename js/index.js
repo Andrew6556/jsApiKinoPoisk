5d2718a4a958dbb1b0635b1137bff1ff3dcbae69
Andrey Kozlov <andrew08552gmail.com>
2023-05-10 01:21:19 +0300

    implemented slider def

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
-// import Card from "../modules/Card.js";
-// import Header from "../modules/Header.js";
-// import Modal from "../modules/Modal.js";
-// import {Slider} from "../modules/slider.js";
+import Card from "../modules/Card.js";
+import Header from "../modules/Header.js";
+import Modal from "../modules/Modal.js";
+import {Slider} from "../modules/slider.js";
 
 
 let path_facts    = (id) => `${id}/facts`,
@@ -17,14 +17,14 @@ let data_movie = [];
 
 
 
-let request = (url="https://kinopoiskapiunofficial.tech/api/v2.2/films/511",rq_type="GET") =>{
+let request = () =>{
     return new Promise((resolve, reject) =>{
         const xhr = new XMLHttpRequest();
-        xhr.open(rq_type, `${url}`, true);
+        xhr.open("GET", `data/info_films.json`, true);
         xhr.onload = () => {
-            if (xhr.status == 200){
+            if (xhr.status == 200 || xhr.status == 201){
                 // console.log(JSON.parse(this.response))
-                resolve(xhr.response)
+                resolve(JSON.parse(xhr.response))
             }else{
                 reject("Errrrrrrrrrrrrrrrrooooooooor")
             }
@@ -36,40 +36,35 @@ let request = (url="https://kinopoiskapiunofficial.tech/api/v2.2/films/511",rq_t
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function get_data_cards() {
-    for (let count = 0; count < 6; count++){
-        // request(films(count)).then(data_film =>{
-        //     return request(path_facts(count))
-        //     .then(fact_film=>{
-        //             data_movie.push({
-        //                 name_movie:data_film.nameRu,
-        //                 id:data_film.kinopoiskId,
-        //                 description:data_film.description,
-        //                 fact:add_fact_about_movie(fact_film),
-        //             })
-        //             div_cards.push(new Card(data_film).wrapper)
-        //             console.log(data_movie)
-        //         }
-        //     )
+function get_data_cards(){
+    request().then(data =>{
+        let div_cards = data.map(item => new Card(item).wrapper)
+        document.querySelector(".header__films").appendChild(new Slider(div_cards).wrapper)
+    })
+        // for (let count = 0; count < 6; count++){
+            // request().then(data_film =>{
+            //     console.log(data_film[count])
+            //     let c = data_film[count]
+            //     div_cards.push(new Card(c).wrapper)
+                // return 
+                // return request(path_facts(count))
+                // .then(fact_film=>{
+                //         data_movie.push({
+                //             name_movie:data_film.nameRu,
+                //             id:data_film.kinopoiskId,
+                //             description:data_film.description,
+                //             fact:add_fact_about_movie(fact_film),
+                //         })
+                //         div_cards.push(new Card(data_film).wrapper)
+                //         console.log(data_movie)
+                //     }
+                // )
         // })
-    }
+    // }
+    
+    
 }
+get_data_cards()
 // document.querySelector(".header__films").appendChild(new Slider(div_cards).wrapper)
 // console.log(get_data_cards())
 // request(films(2))
