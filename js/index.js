7a3c31f3ec858630325d1262f84eb66dad1f6fb2
Andrey Kozlov <andrew08552gmail.com>
2023-05-19 04:46:33 +0300

    removed hanging variable from random fact

--- js/index.js
+++ js/index.js
@@ -65,18 +65,21 @@ document.querySelector(".form").addEventListener("submit", function(link){
 
 function create_slider(path_films, path_trailer, path_img){
     request(path_films).then(films =>{
+        // создаем карточки полученных данных
         let div_card = films.films.map(film => new Card(film).wrapper);
         div_card.forEach(card =>{
             card.querySelector(".card__btn").addEventListener("click", (event) => {
                 let div_title = event.target.closest(".card").querySelector(".card__title").innerText,
                     film_year  = event.target.closest(".card").querySelector(".card__year").innerText,
                     card_info = films.films.find(card => card.nameRu == div_title && card.year == film_year);
-                
+                //нахождения того обьекта по которому был клик
+
                 Promise.all([request(path_trailer(card_info.filmId)),request(path_img(card_info.filmId))]).then(data=>{
                     for (let i = 0; i < 3; i++){
                         document.querySelectorAll(".modalFilm__img-item")[i].src = data[1].items[i].imageUrl
                     }
                     var url = data[0].items[0].url.replace('v', 'embed');
+                    
                     document.querySelector(".modalFilm__title").innerText        = card_info.nameRu;
                     document.querySelector(".modalFilm__description").innerText  = card_info.description;
                     document.querySelector(".modalFilm__video-item").src         = url;
@@ -90,26 +93,34 @@ function create_slider(path_films, path_trailer, path_img){
 
 create_slider(films, films_videos, films_img)
 
+deduce_random_fact()()
+
+
 
 function get_formatted_fact(films_fact){
+    // получаем отредактированный рандомный факт
     let random_fact = mtRandom(0,films_fact.items.length - 1);
     return films_fact.items[random_fact].text.replace(/<\/?[^>]+(>|$)/g, "")
 }
 
 function mtRandom(min, max){
+    // получаем рандоиное число
     return Math.floor(Math.random() * (max - min + 1))
 }
 
-setInterval(function(){
-        let rm_fact_index = mtRandom(0,facts_films.length - 1),
-            random_fact   = get_formatted_fact(facts_films[rm_fact_index])
-        add_fact(random_fact)
-},20000)
+function deduce_random_fact(){
+    let count = 0;
+    return function (){
+        setInterval(function(){
+            let rm_fact_index = mtRandom(0,facts_films.length - 1),
+                random_fact   = get_formatted_fact(facts_films[rm_fact_index])
 
-let count = 0;
-
-function add_fact(random_fact){
-    count++
+            count++
+            add_fact(random_fact, count)
+        },20000)
+    }
+}
+function add_fact(random_fact,count){
     if (count == 1){
         document.querySelector(".FactLoading").classList.add("FactLoading_active");
     }
