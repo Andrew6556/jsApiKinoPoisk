259519341577cef66d9c2bb9d8f14824ff791717
Andrey <andre.kozloff2004@gmail.com>
2023-06-01 02:02:40 +0300

    redesigned the slider logic (simplified)

--- js/index.js
+++ js/index.js
@@ -15,116 +15,50 @@ let keyword_search = name => `http://kinopoiskapiunofficial.tech/api/v2.1/films/
 
 
 
-let params = {
+let options = {
     headers: {
         'X-API-KEY': '4ed6a4de-1c65-48b4-9d9b-922f9cfbd78e',
         'Content-Type': 'application/json; charset=UTF-8',
-    },
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 let header = new Header().wrapper;
 document.querySelector(".wrapper").appendChild(header);
 
-// document.querySelector(".modalFilm__close").addEventListener("click", () =>{
-//     document.querySelector(".modalFilm").classList.toggle("active")
-// })
+document.querySelector(".modalFilm__close").addEventListener("click", () =>{
+    document.querySelector(".modalFilm").classList.toggle("active")
+})
 
-let request = (path) =>{
-    return new Promise((resolve, reject) =>{
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", path, true);
-        xhr.onload = () => {
-            if (xhr.status == 200){
-                resolve(JSON.parse(xhr.response))
-            }else{
-                reject("Errrrrrrrrrrrrrrrrooooooooor")
-            }
-        };
-        xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
-        xhr.setRequestHeader("X-API-KEY", '4ed6a4de-1c65-48b4-9d9b-922f9cfbd78e');
-        xhr.send();
-    })
-}
+async function create_slider(path_trailer, path_img){
+    let response_films = await fetch(films, options),
+        initial_films  = await response_films.json();
 
+    let div_card = initial_films.films.map(film  => new Card(film).wrapper);
 
+    div_card.forEach(card => {
+        card.querySelector(".card__btn").addEventListener("click", async (event) => {
+            let div_title = event.target.closest(".card").querySelector(".card__title").innerText,
+                film_year = event.target.closest(".card").querySelector(".card__year").innerText,
+                card_info = initial_films.films.find(film => film.nameRu == div_title && film.year == film_year);
+                //нахождения того обьекта ,по которому был клик
+                let [response_trailer, response_pictures] = await Promise.all([
+                                fetch(path_trailer(card_info.filmId), options),
+                                fetch(path_img(card_info.filmId),     options)])
 
-// document.querySelector(".form").addEventListener("submit", function(link){
-//     link.preventDefault();
-//     let value_search = this.querySelector(".header__searchBox").value;
-//     request(keyword_search(value_search)).then(search_result =>{
-//         if (search_result.films.length != 0){
-//             document.querySelector(".header__NothingFound").classList.add("header__NothingFound_active")
-//             if(document.querySelector(".slider") != null){
-//                 document.querySelector(".slider").remove()
-//             }
-//             create_slider(keyword_search(value_search), films_videos, films_img)
-//         }else{
-//             document.querySelector(".slider").remove()
-//             document.querySelector(".header__NothingFound").classList.remove("header__NothingFound_active")
-//         }
-//     })
-// })
-
-
-function create_slider(path_films, path_trailer, path_img){
-    request(path_films).then(films =>{
-        // создаем карточки из полученных данных
-        let div_card = films.films.map(film => new Card(film).wrapper);
-        div_card.forEach(card =>{
-            card.querySelector(".card__btn").addEventListener("click", (event) => {
-                let div_title = event.target.closest(".card").querySelector(".card__title").innerText,
-                    film_year  = event.target.closest(".card").querySelector(".card__year").innerText,
-                    card_info = films.films.find(film => film.nameRu == div_title && film.year == film_year);
-                    //нахождения того обьекта ,по которому был клик
-
-                Promise.all([request(path_trailer(card_info.filmId)),request(path_img(card_info.filmId))]).then(data=>{
-                    adding_pictures_modal(data[1])
-                    
-                    for(let i = 0 ; i < data[0].items.length; i++){
-                        // Находим только ссылку на ютуб
-                        if(data[0].items[i].url.search("https://www.youtube.com/") == 0){
-                            var url = data[0].items[i].url.search("/v/") == -1 ? data[0].items[i].url.replace('watch?v=', 'embed/'):
-                                                                            data[0].items[i].url.replace('/v/', '/embed/')
-                            // форматируем ее чтоб была рабочей
-                            break
-                        }
-                    }
-                    document.querySelector(".modalFilm__title").innerText        = card_info.nameRu;
-                    document.querySelector(".modalFilm__description").innerText  = card_info.description;
-                    document.querySelector(".modalFilm__video-item").src         = url;
-                })
-            })
+                let movie_trailer = await response_trailer.json(),
+                    movie_picture = await response_pictures.json();
+                
+                adding_pictures_modal(movie_picture)
+                
+                document.querySelector(".modalFilm__title").innerText        = card_info.nameRu;
+                document.querySelector(".modalFilm__description").innerText  = card_info.description;
+                document.querySelector(".modalFilm__video-item").src         = get_working_link(movie_trailer);
         })
-        document.querySelector(".header__films").appendChild(new Slider(div_card).wrapper)
     })
+    return div_card
 }
+
 function adding_pictures_modal(images){
     // при отсутвие допустимого значения картинок, скрываем блок modalFilm__galley
     if(images.items.length > 3){
@@ -135,16 +69,6 @@ function adding_pictures_modal(images){
     }else{
         document.querySelector(".modalFilm__galley").style.display = 'none';
     }
-
-}
-
-create_slider(films, films_videos, films_img)
-
-
-function get_formatted_fact(films_fact){
-    // получаем отредактированный рандомный факт
-    let random_fact = mtRandom(0,films_fact.items.length - 1);
-    return films_fact.items[random_fact].text.replace(/<\/?[^>]+(>|$)/g, "")
 }
 
 function mtRandom(min, max){
@@ -152,9 +76,52 @@ function mtRandom(min, max){
     return Math.floor(Math.random() * (max - min + 1))
 }
 
+function get_formatted_fact(films_fact){
+    // получаем отредактированный рандомный факт
+    let random_fact = mtRandom(0,films_fact.items.length - 1);
+    return films_fact.items[random_fact].text.replace(/<\/?[^>]+(>|$)/g, "")
+}
+
+function get_working_link(links){
+    let formatted_link;
+    for(let i = 0 ; i < links.items.length; i++){
+        // Находим только ссылку на ютуб
+        if(links.items[i].url.search("https://www.youtube.com/") == 0){
+            // форматируем ее чтоб была рабочей
+            formatted_link = links.items[i].url.search("/v/") == -1 ? links.items[i].url.replace('watch?v=', 'embed/'):
+                                                                        links.items[i].url.replace('/v/', '/embed/');
+            break
+        }
+    }
+    return formatted_link
+}
+
+create_slider(films_videos, films_img)
+    .then(div_cards => document.querySelector(".header__films").appendChild(new Slider(div_cards).wrapper))
+
+
+document.querySelector(".form").addEventListener("submit", function(link){
+    link.preventDefault();
+    let value_search = this.querySelector(".header__searchBox").value;
+    request(keyword_search(value_search)).then(search_result =>{
+        if (search_result.films.length != 0){
+            document.querySelector(".header__NothingFound").classList.add("header__NothingFound_active")
+            if(document.querySelector(".slider") != null){
+                document.querySelector(".slider").remove()
+            }
+            create_slider(keyword_search(value_search), films_videos, films_img)
+        }else{
+            document.querySelector(".slider").remove()
+            document.querySelector(".header__NothingFound").classList.remove("header__NothingFound_active")
+        }
+    })
+})
+
+
+
 async function deduce_random_fact(){
     return await new Promise(resolve => setInterval(async function(){
-        let get_data   = await fetch(path_facts, params),
+        let get_data   = await fetch(path_facts, options),
             facts_films = await get_data.json();
 
         let rm_fact_index = mtRandom(0,facts_films.length - 1);
@@ -167,3 +134,7 @@ deduce_random_fact().then(random_fact =>{
     }
     document.querySelector(".header__FactFilm-text").innerText = random_fact;
 })
+
+
+
+
